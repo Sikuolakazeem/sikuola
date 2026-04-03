511c7364f95d2a50ff10705de0f5f147b38c299f
Sikuolakazeem <sikuola16@gmail.com>
2026-04-03 15:17:43 +0100

    fixed routing issues

--- src/normal/Header.jsx
+++ src/normal/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="w-[80%] flex mx-auto items-center justify-end mt-20">
       <div className=" mt-3 fixed top-1 z-10 cursor-pointer">
-        <BtnSwitch onClick={() => navigate("/", { replace: true })}>
+        <BtnSwitch onClick={() => navigate("/terminal", { replace: true })}>
           <span className="text-3xl text-text-faint">&gt;</span>
           <p className="hidden md:block text-[0.85rem] text-text-faint">
             Switch to Terminal
